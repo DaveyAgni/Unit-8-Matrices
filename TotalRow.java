9d87ac11b91ddf1399cead5f44fc9984f4ab5ff4
DaveyAgni <devarchit.agnihotri@icloud.com>
2025-02-20 08:44:38 -0500

    Update TotalRow.java

--- TotalRow.java
+++ TotalRow.java
@@ -8,7 +8,25 @@ public class TotalRow
 {
     public static List<Integer> getRowTotals( int[][] m )
     {
-    	//add code here
-		return null;
+    	int total = 0;
+        int[][]tot = {{m.length - 1},{0}};
+
+        for(int i = 0; i < m.length; i++)
+        {
+            for(int c = 0; c < m[i].length; c++)
+            {
+                total += m[i][c];
+            }
+            tot[i][0] = total;
+            total = 0;
+        }
+
+        List<Integer> list = new ArrayList<Integer>();
+        for(int d = 0; d < tot.length; d++)
+        {
+            list.add(tot[d][0]);
+        }
+		
+        return list;
     }
 }
