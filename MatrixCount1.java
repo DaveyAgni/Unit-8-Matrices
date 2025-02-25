1b4c652c22002569ce50df1639c7d718e37e5d53
DaveyAgni <devarchit.agnihotri@icloud.com>
2025-02-25 09:18:49 -0500

    Update MatrixCount1.java

--- MatrixCount1.java
+++ MatrixCount1.java
@@ -8,14 +8,27 @@ import java.io.*;
 public class MatrixCount1
 {
     private static int[][] m = {{ 1, 2, 3, 4, 5},
-    										{ 6, 7, 8, 9, 0},
-    										{ 6, 7, 1, 2, 5},
-    										{ 6, 7, 8, 9, 0},
-    										{ 5, 4, 3, 2, 1}};
+    							{ 6, 7, 8, 9, 0},
+    							{ 6, 7, 1, 2, 5},
+    							{ 6, 7, 8, 9, 0},
+    							{ 5, 4, 3, 2, 1}};
 
     public static int count( int val  )
     {
-		//add code
-		return 0;
+        int count = 0;
+
+		for(int i = 0; i < m.length; i++)
+        {
+            for(int c = 0; c < m[i].length; c++)
+            {
+                if(m[i][c] == val)
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
     }
 }
